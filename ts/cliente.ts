24cf29e7250a037e71e6acc22b910155b8fcefc8
sahon <sahon.serra@etec.sp.gov.br>
2025-03-14 21:52:24 -0300

    ainda odeio essa aula

---
+++ ts/cliente.ts
@@ -0,0 +1,41 @@
+namespace empresa{
+    export class cliente extends Pessoa{
+        private _codigo:number;
+        private _saldo:number = 0;
+
+
+        constructor(codigo:number){
+            super();
+            this._codigo = codigo;
+        }
+
+        get codigo(){
+            return this._codigo;
+
+        }
+
+        get saldo(){
+            return this._saldo;
+        }
+
+        public deposita (valor:number){
+            this._saldo += valor;
+        }
+
+        public comprar (valorCompra:number):boolean{
+            if(this._saldo >= valorCompra){
+                this._saldo = this._saldo - valorCompra;
+                return true;
+
+            }else{
+                return false;
+            }
+           
+        }
+
+
+
+    }
+
+    
+}
